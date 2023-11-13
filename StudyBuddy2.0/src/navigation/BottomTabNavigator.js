48e33e7c0d520ff1636d31f53456f114eacea9fd
Sisi Small <smallsianah@gmail.com>
2023-11-12 23:10:38 -0500

    profile screen started

--- StudyBuddy2.0/src/navigation/BottomTabNavigator.js
+++ StudyBuddy2.0/src/navigation/BottomTabNavigator.js
@@ -1,32 +1,46 @@
-// In src/navigation/BottomTabNavigator.js
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomePage from '../screens/HomePage';
 import MessagesScreen from '../screens/MessagesScreen';
+import ProfileScreen from '../screens/ProfileScreen'; // Import ProfileScreen
 import { Ionicons } from '@expo/vector-icons';
-
-// Import other screens you want to include in the bottom tabs
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => (
   <Tab.Navigator
-  screenOptions={{
-    headerShown: false, // This hides the header for all tab screens
-  }}>
-    {/* Define tabs and their associated screens here */}
-    <Tab.Screen name="HomePage" component={HomePage} /* ... */ />
-    {/* ... other tabs */}
+    screenOptions={{
+      headerShown: false, // This hides the header for all tab screens
+    }}>
+    <Tab.Screen 
+      name="HomePage" 
+      component={HomePage}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="home-outline" color={color} size={size} />
+        ),
+      }} 
+    />
     <Tab.Screen
-  name="Messages"
-  component={MessagesScreen}
-  options={{
-    tabBarIcon: ({ color, size }) => (
-      <Ionicons name="chatbubble-outline" color={color} size={size} />
-    ),
-  }}
-/>
+      name="Messages"
+      component={MessagesScreen}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="chatbubble-outline" color={color} size={size} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Profile"
+      component={ProfileScreen}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="person-outline" color={color} size={size} />
+        ),
+      }}
+    />
   </Tab.Navigator>
 );
 
 export default BottomTabNavigator;
+
 
---
+++ StudyBuddy2.0/src/screens/ProfileScreen.js
@@ -0,0 +1,162 @@
+import React, { useState } from 'react';
+import { 
+  View, 
+  Text, 
+  TextInput, 
+  StyleSheet, 
+  Image, 
+  ScrollView, 
+  TouchableOpacity, 
+  FlatList 
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+const ProfileScreen = () => {
+  // Dummy data for the example. Replace with state and context where necessary.
+  const userInfo = {
+    fullName: 'Billy McBillerson',
+    email: 'billysilly@gmail.com',
+    profilePic: 'https://via.placeholder.com/150', // Replace with actual image path
+    studyGroups: ['Study Group 1', 'Study Group 2'],
+    classes: ['Class 1', 'Class 2'],
+    // Additional sections like 'Likes', 'Skills', etc. would be handled similarly
+  };
+
+  // State to handle the addition and removal of classes.
+  const [classes, setClasses] = useState(userInfo.classes);
+  const [newClass, setNewClass] = useState('');
+
+  const handleAddClass = () => {
+    setClasses([...classes, newClass]);
+    setNewClass('');
+  };
+
+  const handleRemoveClass = (index) => {
+    const updatedClasses = classes.filter((_, classIndex) => classIndex !== index);
+    setClasses(updatedClasses);
+  };
+
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => {}}>
+          <Ionicons name="arrow-back" size={24} color="#000" />
+        </TouchableOpacity>
+        <Text style={styles.headerTitle}>{userInfo.fullName}</Text>
+        <TouchableOpacity onPress={() => {}}>
+          <Text>Logout</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.profileSection}>
+        <Image source={{ uri: userInfo.profilePic }} style={styles.profilePic} />
+        <View style={styles.infoSection}>
+          <Text style={styles.infoText}>Email: {userInfo.email}</Text>
+          <Text style={styles.infoText}>Name: {userInfo.fullName}</Text>
+        </View>
+      </View>
+
+      <Text style={styles.sectionTitle}>Study Groups</Text>
+      <FlatList
+        data={userInfo.studyGroups}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => <Text style={styles.listItem}>{item}</Text>}
+        horizontal
+      />
+
+      <Text style={styles.sectionTitle}>Classes</Text>
+      <View style={styles.classesList}>
+        {classes.map((classItem, index) => (
+          <View key={index} style={styles.classItem}>
+            <Text style={styles.listItem}>{classItem}</Text>
+            <TouchableOpacity onPress={() => handleRemoveClass(index)}>
+              <Ionicons name="remove-circle" size={24} color="#000" />
+            </TouchableOpacity>
+          </View>
+        ))}
+      </View>
+      <View style={styles.addClassSection}>
+        <TextInput
+          style={styles.classInput}
+          placeholder="New class"
+          value={newClass}
+          onChangeText={setNewClass}
+        />
+        <TouchableOpacity onPress={handleAddClass}>
+          <Ionicons name="add-circle" size={24} color="#000" />
+        </TouchableOpacity>
+      </View>
+      
+      {/* Additional sections would follow a similar pattern to 'Classes' */}
+      
+      {/* Rest of the profile sections... */}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 20,
+  },
+  headerTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  profileSection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 20,
+  },
+  profilePic: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  infoSection: {
+    marginLeft: 20,
+  },
+  infoText: {
+    fontSize: 16,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  listItem: {
+    margin: 10,
+  },
+  classesList: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingHorizontal: 20,
+  },
+  classItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 10,
+  },
+  addClassSection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  classInput: {
+    borderBottomWidth: 1,
+    flex: 1,
+    marginRight: 10,
+  },
+  // Define additional styles as needed
+});
+
+export default ProfileScreen;
+
+
